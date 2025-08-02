51c6f7b73c314a50756be99a375229ad045bfa35
Juan D. Jara <juanorigami@gmail.com>
2025-08-03 01:04:07 +0200

    this is serious now

--- app/request.ts
+++ app/request.ts
@@ -1,10 +1,14 @@
-export async function getJSON<T>(url: string) {
-  const res = await fetch(url)
-  if (!res.ok) {
-    throw new Error(`Request to ${url} failed with status ${res.status} ${res.statusText}\n${await res.text()}`)
-  }
+import { parseHTML } from 'linkedom'
 
-  const json = await res.json()
+export async function getJSON<T>(url: string) {
+  // const res = await fetch(url)
+  // if (!res.ok) {
+  //   throw new Error(`Request to ${url} failed with status ${res.status} ${res.statusText}\n${await res.text()}`)
+  // }
+
+  // const json = await res.json()
+  // return json as T
+  const json = await flaresolverrGet(url)
   return json as T
 }
 
@@ -20,4 +24,37 @@ export async function tryGetJSON<T, T2 = T>(initialData: T2, url: string) {
 
 export function wrapData<T, E>(data: T, error?: E) {
   return { data, error }
+}
+
+async function flaresolverrGet(url: string) {
+  const flaresolverrUrl = process.env.FLARESOLVERR_URL
+  if (!flaresolverrUrl) {
+    throw new Error('FLARESOLVERR_URL is not set')
+  }
+
+  const res = await fetch(`${flaresolverrUrl}/v1`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      cmd: 'request.get',
+      url,
+      maxTimeout: 10000, // 10 seconds
+    })
+  })
+  if (!res.ok) {
+    throw new Error(`Request to ${url} (and flaresolverr) failed with status ${res.status} ${res.statusText}\n${await res.text()}`)
+  }
+
+  const jsonRes = await res.json()
+  const html = jsonRes.solution?.response as string
+  const { document } = parseHTML(html)
+  const json = document.querySelector('pre')?.textContent
+  if (!json) {
+    throw new Error('No JSON found in the HTML response')
+  }
+
+  return JSON.parse(json)
 }
