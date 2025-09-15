d477cdf94ee6d641fe4080f318d626a85ace4b86
Juan D. Jara <juanorigami@gmail.com>
2025-09-15 17:12:30 +0200

    group last read by folder

--- app/components/LastRead.tsx
+++ app/components/LastRead.tsx
@@ -1,11 +1,26 @@
 import { Form, Link, useLoaderData, useNavigation } from '@remix-run/react'
 import { loader } from '@/routes/_index'
 import { IconClose } from './icons'
+import { useMemo } from 'react'
 
 export default function LastRead() {
   const { lastRead } = useLoaderData<typeof loader>()
   const { state } = useNavigation()
   const busy = state !== 'idle'
+  const folders = useMemo(() => {
+    const values = {} as Record<string, string[]>
+    for (const file of lastRead) {
+      const folder = file.split('/')[1]
+      if (!values[folder]) {
+        values[folder] = []
+      }
+      values[folder].push(file)
+    }
+    return Object.entries(values).map(([key, value]) => ({
+      folder: key,
+      items: value
+    }))
+  }, [lastRead])
 
   if (!lastRead?.length) {
     return null
@@ -28,21 +43,26 @@ export default function LastRead() {
           </button>
         </Form>
       </div>
-      <ul className="divide-y">
-        {lastRead.map((f) => (
-          <li
-            key={f}
-            className="flex items-stretch gap-2 hover:bg-gray-100 transition-colors"
-          >
-            <Link
-              className="block w-full p-3"
-              to={`/read?file=${encodeURIComponent(f)}`}
-            >
-              {f.split('/').pop()}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {folders.map(({ folder, items }) => (
+        <details open key={folder} className='px-3 py-2'>
+          <summary className='text-lg mb-2'>{folder}</summary>
+          <ul className="divide-y px-1">
+            {items.map((f) => (
+              <li
+                key={f}
+                className="flex items-stretch gap-2 hover:bg-gray-100 transition-colors"
+              >
+                <Link
+                  className="block w-full p-3"
+                  to={`/read?file=${encodeURIComponent(f)}`}
+                >
+                  {f.split('/').pop()}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </details>
+      ))}
     </div>
   )
 }
